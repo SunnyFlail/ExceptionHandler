cd62983008a326b05ef08c658bb76ff32bc2abe8
Patryk Ćwikliński <82274752+SunnyFlail@users.noreply.github.com>
2021-06-22 13:32:11 +0200

    Update import.php

--- import.php
+++ import.php
@@ -3,4 +3,4 @@
 use SunnyFlail\ExceptionHandler\ExceptionHandler;
 
 set_error_handler([ExceptionHandler::class, "handleError"]);
-set_exception_handler([SunnyFlail\ExceptionHandler\ExceptionHandler::class, "handleException"]);
+set_exception_handler([ExceptionHandler::class, "handleException"]);
